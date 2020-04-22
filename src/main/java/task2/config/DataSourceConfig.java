f0aac88072dcb99f4810ed53928e818e5ba11553
Андрей <slalomist22@yandex.ru>
2020-04-22 13:03:33 +0400

    oracle || mysql || postgresql

--- src/main/java/task2/config/DataSourceConfig.java
+++ src/main/java/task2/config/DataSourceConfig.java
@@ -3,6 +3,7 @@ package task2.config;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
 
 import static task2.config.FindProperty.getProperty;
@@ -13,19 +14,45 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() {
         System.out.println("Создаем свой DataSours");
-        String ip = getProperty("db.ip");
-        String port = getProperty("db.port");
-        String sid = getProperty("db.SID");
-        String username = getProperty("db.username");
-        String password = getProperty("db.Password");
 
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("oracle.jdbc.driver.OracleDriver");
-        dataSourceBuilder.url("jdbc:oracle:thin:@" + ip + ":" + port + ":" + sid);
-        dataSourceBuilder.username(username);
-        dataSourceBuilder.password(password);
-
-        return dataSourceBuilder.build();
+        String dbType = getProperty("db.Type");
+        return DataSourceBuilder.create()
+                .driverClassName(geDriverClassName(dbType))
+                .url(getUrl(dbType))
+                .username(getProperty("db.username"))
+                .password(getProperty("db.Password")).build();
     }
 
+    private String getUrl(String dbType) {
+
+        String ip = getProperty("db.ip");
+        String port = getProperty("db.port");
+        String baseName = getProperty("db.baseName.SID");
+        if (ip.equals("") || port.equals("") || baseName.equals("")) {
+            return getProperty("db.url");
+        }
+        switch (dbType) {
+            case "mysql":
+                return "jdbc:mysql://" + ip + ":" + port + "/" + baseName;
+            case "oracle":
+                return "jdbc:oracle:thin:@" + ip + ":" + port + ":" + baseName;
+            case "postgresql":
+                return "jdbc:postgresql://" + ip + ":" + port + "/" + baseName;
+            default:
+                return getProperty("db.url");
+        }
+    }
+
+    private String geDriverClassName(String dbType) {
+        switch (dbType) {
+            case "mysql":
+                return "com.mysql.jdbc.Driver";
+            case "oracle":
+                return "oracle.jdbc.driver.OracleDriver";
+            case "postgresql":
+                return "org.postgresql.Driver";
+            default:
+                return getProperty("db.driverClassName");
+        }
+    }
 }
